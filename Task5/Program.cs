2184a52ac714f2bf9c9422796d44463dbb9e68e7
Anastasiia <Batrakovaad@yndex.ru>
2023-02-13 23:34:26 +0700

    Finish

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -4,3 +4,13 @@
 // в промежутке от -N до N.
 //4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
 //2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("Enter a number ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = -1 * Math.Abs(a);
+
+while(i <= Math.Abs(a))
+{
+    Console.WriteLine(i);
+    i++;
+} 
