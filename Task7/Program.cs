9e83d37fd793eb66ff364317ec2ba1fdcb109398
Anastasiia <Batrakovaad@yndex.ru>
2023-02-13 23:53:51 +0700

    finishTask7

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, 
+//которая принимает на вход трёхзначное число
+// и на выходе показывает последнюю цифру этого числа.
+//456 -> 6
+//782 -> 2
+//918 -> 8
+
+Console.WriteLine("Enter a number ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a / 100 != 0 && a / 100 < 9 && a / 100 > 1)
+{
+    int result = a % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("This number isn't correct");
+}
