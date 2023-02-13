ab4f8e2049bd6892eb0beaacb9fb51d448c2efd6
Anastasiia <Batrakovaad@yndex.ru>
2023-02-13 22:50:40 +0700

    SecondSolution

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -7,30 +7,35 @@
 Console.WriteLine("Enter a number ");
 int a = Convert.ToInt32(Console.ReadLine());
 
-switch (a)
+if(a == 1)
 {
-    case 1:
-        Console.WriteLine("Monday");
-        break;
-    case 2:
-        Console.WriteLine("Tuesday");
-        break;
-    case 3:
-        Console.WriteLine("Wednesday");
-        break;
-    case 4:
-        Console.WriteLine("Thursday");
-        break;
-    case 5:
-        Console.WriteLine("Friday");
-        break;
-    case 6:
-        Console.WriteLine("Saturday");
-        break;
-    case 7:
-        Console.WriteLine("Sunday");
-        break;
-    default:
-        Console.WriteLine("Enter from 1 to 7");
-        break;
+    Console.WriteLine("Monday");
+}
+else if(a == 2)
+ {
+   Console.WriteLine("Tuesday");
+ }
+else if(a == 3)
+ {   
+    Console.WriteLine("Wednesday");
+ }
+ else if(a ==4) 
+ {   
+    Console.WriteLine("Thursday");
+ }
+else if(a == 5)
+ {
+   Console.WriteLine("Friday");
+ }
+ else if(a == 6)
+ {
+   Console.WriteLine("Saturday");
+ }
+ else if(a ==7)
+ {
+     Console.WriteLine("Sunday");
+ }
+ else
+ {
+   Console.WriteLine("Enter from 1 to 7");
 }
