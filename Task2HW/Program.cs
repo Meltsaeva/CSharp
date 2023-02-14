56578a17dc06b103a6b99d445c80de74f9995b6c
Anastasiia <Batrakovaad@yndex.ru>
2023-02-14 15:54:25 +0700

    Finish2HW

--- Task2HW/Program.cs
+++ Task2HW/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, 
+//которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+
+Console.WriteLine("Enter The First Number ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter The Second Number ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+int min = b;
+
+if (a > max) max = a;
+if (b > max) max = b;
+
+if (a < max) min = a;
+if (b < max) min = b;
+
+Console.WriteLine("max= ");
+Console.WriteLine(max);
+Console.WriteLine("min= ");
+Console.WriteLine(min);
