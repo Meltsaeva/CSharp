3dddebeabfd787f07f46db6a3c02cc0ab45fc8fe
Anastasiia <Batrakovaad@yndex.ru>
2023-02-16 15:02:47 +0700

    finish

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, 
+//которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+
+Console.WriteLine("Input a number ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 100 || a > 999)
+{
+    Console.WriteLine("Incorrect");
+}
+
+else 
+{
+    int b = a /10 % 10;
+    Console.WriteLine(b);
+}
