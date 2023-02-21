a16b6a8150291c3836adb30d82ad1b26f2a48b1e
Anastasiia <Batrakovaad@yndex.ru>
2023-02-21 22:02:15 +0700

    finish17

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, 
+//которая принимает на вход координаты точки (X и Y),
+//причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, 
+//в которой находится эта точка.
+
+
+Console.WriteLine("Input X ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input Y ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x == 0 || y == 0)
+{
+    Console.WriteLine("Input X != 0 and Y != 0");
+}
+else if(x > 0 && y > 0)
+{
+    Console.WriteLine("I");
+}
+else if(x < 0 && y > 0)
+{
+    Console.WriteLine("II");
+}
+else if(x < 0 && y < 0)
+{
+    Console.WriteLine("III");
+}
+else
+{
+    Console.WriteLine("IV");
+}
