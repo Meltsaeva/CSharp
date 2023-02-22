d5c44670d0fac13fd6cea91a64a42fb3f50486bc
Anastasiia <Batrakovaad@yndex.ru>
2023-02-22 12:33:42 +0700

    Finish21

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -6,6 +6,8 @@
 //A (3,6); B (2,1) -> 5,09
 //A (7,-5); B (1,-1) -> 7,21
 
+//double[] a = new double[2];
+
 Console.WriteLine("Input x1");
 double x1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input y1");
@@ -20,3 +22,4 @@ double x3 = Math.Pow(x2 - x1, 2);
 double y3 = Math.Pow(y2 - y1, 2);
 double distance = Math.Sqrt(x3 + y3);
 Console.WriteLine($"The interval between the two points is {distance}");
+
---
+++ Task21/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, 
+//которая принимает на вход пятизначное число 
+//и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.WriteLine("Input a number ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100000 && number <10000)
+{
+    Console.WriteLine("Incorrect");
+}
+
+else 
+{
+    int a = number / 10000;
+    int b = number / 1000 % 10;
+    int c = number / 100 % 10;
+    int d = number / 10 % 10;
+    int e = number % 10;
+    if (a == e && b == d)
+    Console.WriteLine("It's actually a palindrome, guys");
+    else 
+    Console.WriteLine("It isn't a palindrome, guys");
+}
