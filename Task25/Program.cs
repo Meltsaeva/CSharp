8d014207e19980cc67ab93a8ef207d0544a74574
Anastasiia <Batrakovaad@yndex.ru>
2023-02-25 21:10:08 +0700

    Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,56 @@
+﻿
+// Напишите программу, 
+// которая принимает на вход число и 
+// выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+#region 
+
+
+int number = Prompt("Input a number");
+Count(number);
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void Count(int userNumber)
+{
+    char[] number = userNumber.ToString().ToCharArray();
+    Console.WriteLine($"{number.Length}");
+}
+
+#endregion
+
+// Напишите программу, 
+// которая принимает на вход число N и 
+// выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+#region 
+
+int number1 = GetNumber();
+Console.WriteLine($"Product is {Pro(number1)}");
+
+int GetNumber()
+{
+    Console.WriteLine("Input a number");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    return number1;
+}   
+int Pro(int a) 
+{
+    int result = 1;
+    for (int i = 1; i <= a; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+#endregion
