b82fb91f25205c8e5f83229d52fccb1d725eb29e
Anastasiia <Batrakovaad@yndex.ru>
2023-02-25 21:27:11 +0700

    finish

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -5,52 +5,85 @@
 // 456 -> 3
 // 78 -> 2
 // 89126 -> 5
-#region 
+// #region 
 
 
-int number = Prompt("Input a number");
-Count(number);
+// int number = Prompt("Input a number");
+// Count(number);
 
-int Prompt(string message)
-{
-    Console.WriteLine(message);
-    int result = Convert.ToInt32(Console.ReadLine());
-    return result;
-}
+// int Prompt(string message)
+// {
+//     Console.WriteLine(message);
+//     int result = Convert.ToInt32(Console.ReadLine());
+//     return result;
+// }
 
-void Count(int userNumber)
-{
-    char[] number = userNumber.ToString().ToCharArray();
-    Console.WriteLine($"{number.Length}");
-}
+// void Count(int userNumber)
+// {
+//     char[] number = userNumber.ToString().ToCharArray();
+//     Console.WriteLine($"{number.Length}");
+// }
 
-#endregion
+// #endregion
 
-// Напишите программу, 
-// которая принимает на вход число N и 
-// выдаёт произведение чисел от 1 до N.
-// 4 -> 24
-// 5 -> 120
+// // Напишите программу, 
+// // которая принимает на вход число N и 
+// // выдаёт произведение чисел от 1 до N.
+// // 4 -> 24
+// // 5 -> 120
 
-#region 
+// #region 
 
-int number1 = GetNumber();
-Console.WriteLine($"Product is {Pro(number1)}");
+// int number1 = GetNumber();
+// Console.WriteLine($"Product is {Pro(number1)}");
+
+// int GetNumber()
+// {
+//     Console.WriteLine("Input a number");
+//     int number1 = Convert.ToInt32(Console.ReadLine());
+//     return number1;
+// }   
+// int Pro(int a) 
+// {
+//     int result = 1;
+//     for (int i = 1; i <= a; i++)
+//     {
+//         result *= i;
+//     }
+//     return result;
+// }
+
+// #endregion
+
 
 int GetNumber()
 {
     Console.WriteLine("Input a number");
-    int number1 = Convert.ToInt32(Console.ReadLine());
-    return number1;
-}   
-int Pro(int a) 
-{
-    int result = 1;
-    for (int i = 1; i <= a; i++)
-    {
-        result *= i;
-    }
-    return result;
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
 }
 
-#endregion
+int Numbers()
+{
+    int a = GetNumber();
+    int count = 0;
+    while (a > 0)
+    {
+        a = a / 10;
+        count ++;
+    }
+    return count;
+}
+ 
+int Result()
+{
+    int b = GetNumber();
+    int x = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        x = x*i;
+    }
+    return x;
+}
+
+Console.WriteLine(Result()); // Result or Numbers
